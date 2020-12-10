d41035eb367552ac8ff2579ec18b78b62416f0af
haskinsm <60662825+haskinsm@users.noreply.github.com>
2020-12-10 13:23:32 +0000

    SENG: Added GitHub username variable which makes it far easier to change the base GitHub user you want to interrogate. i.e. Only have to change one variable now rather than a load

--- SENG Intergogate GitHub API.R
+++ SENG Intergogate GitHub API.R
@@ -1,13 +1,16 @@
-##Below are packages necessary for program:
-install.packages("jsonlite")
+## Reference: The first 30 or so lines were modified from the src:
+## https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
+
+##Below are packages necessary for program, installs only need to be run once:
+#install.packages("jsonlite")
 library(jsonlite)
-install.packages("httpuv")
+#install.packages("httpuv")
 library(httpuv)
-install.packages("httr")
+#install.packages("httr")
 library(httr)
 
-# Can be github, linkedin etc depending on application 
-##oauth_endpoints("github") ## This sets the API we want to interrgogate as being GitHub
+
+gitHubUsername = "phadej" ##Makes it easy to change the username of base GitHub user we want to interrogate
 
 GitHubSENGMeasApp <- oauth_app(appname = "SENG_Meas_App", ## My GitHub App Link: https://github.com/settings/applications/1429513
                    key = "b9f9199e609b083310c1", ##Client ID of APP
@@ -18,7 +21,8 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), GitHubSENGMeasApp)
 
 # Use API
 gtoken <- config(token = github_token)
-APIResponse <- GET("https://api.github.com/users/phadej/repos", gtoken)
+APIResponse <- GET(paste("https://api.github.com/users/", gitHubUsername,"/repos", sep = ""), gtoken)
+## The paste function above functions like 
 
 # Take action on http error
 stop_for_status(APIResponse)
@@ -30,11 +34,13 @@ JSONData = content(APIResponse)
 gitDataFrame = jsonlite::fromJSON(jsonlite::toJSON(JSONData))
 
 # Subset data.frame
-gitDataFrame[gitDataFrame$full_name == "phadej/datasharing", "created_at"] 
+gitDataFrame[gitDataFrame$full_name == paste(gitHubUsername,"/datasharing"), "created_at"] 
 
-data = fromJSON("https://api.github.com/users/phadej")
+data = fromJSON(paste("https://api.github.com/users/",gitHubUsername))
 data ## This will display all the info/data that is returned from the get request
 
 ## Data can be easily accessed as is done below: 
 data$public_repos
 data$followers
+
+
